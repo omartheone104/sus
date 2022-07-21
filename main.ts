a14fd0782d895cd2274c404b8665f9c8b92e66cf
omartheone104 <79159097+omartheone104@users.noreply.github.com>
2022-07-21 10:38:33 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,33 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    if (player.get(LedSpriteProperty.Y) == 4 && !enemy.isTouching(player)) {
+input.onButtonPressed(Button.A, function () {
+    if (player.get(LedSpriteProperty.Y) == 4 && !(enemy.isTouching(player))) {
         player.set(LedSpriteProperty.Y, 0)
     } else {
         player.change(LedSpriteProperty.Y, 1)
     }
-    
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    if (player.get(LedSpriteProperty.X) == 4 && !enemy.isTouching(player)) {
+input.onButtonPressed(Button.B, function () {
+    if (player.get(LedSpriteProperty.X) == 4 && !(enemy.isTouching(player))) {
         player.set(LedSpriteProperty.X, 0)
     } else {
         player.change(LedSpriteProperty.X, 1)
     }
-    
 })
-let enemy : game.LedSprite = null
-let player : game.LedSprite = null
+let enemy: game.LedSprite = null
+let player: game.LedSprite = null
 basic.showString("PACMAN")
 let pacman = images.createBigImage(`
     . # # # . . . # # .
-        # . . . # . # . . #
-        # . . . # . # . # .
-        # # . # # . # . . #
-        # . # . # . . # # .
-`)
+    # . . . # . # . . #
+    # . . . # . # . # .
+    # # . # # . # . . #
+    # . # . # . . # # .
+    `)
 pacman.scrollImage(1, 200)
 player = game.createSprite(2, 4)
 enemy = game.createSprite(2, 2)
 let food = game.createSprite(randint(0, 4), randint(0, 4))
 game.setScore(0)
-basic.forever(function on_forever() {
+basic.forever(function () {
     basic.pause(200)
     enemy.change(LedSpriteProperty.X, randint(-1, 1))
     enemy.change(LedSpriteProperty.Y, randint(-1, 1))
@@ -38,10 +36,8 @@ basic.forever(function on_forever() {
         food.change(LedSpriteProperty.X, randint(-4, 4))
         food.change(LedSpriteProperty.Y, randint(-4, 4))
     }
-    
     if (player.isTouching(enemy)) {
         game.gameOver()
         basic.showNumber(game.score())
     }
-    
 })
